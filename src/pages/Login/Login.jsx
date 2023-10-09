cb1eced226c7ad7ea0e69f3164a3cd7055ea3cc8
ranamiah221 <ranarasul21@gmail.com>
2023-10-09 11:42:10 +0600

    added login and register form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,30 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     return (
         <div>
-            <h1>This is login</h1>
+           <div className="w-1/2 mx-auto border p-10 rounded-lg bg-green-200 mt-5">
+            <h1 className="text-4xl font-bold text-center underline text-blue-600 mb-3">Please Login</h1>
+             <form>
+             <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                 </label>
+                    <input type="email" placeholder="email" className="input input-bordered" required />
+             </div>
+             <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                 </label>
+                   <input type="password" placeholder="password" className="input input-bordered" required />
+             </div>
+             <div className="form-control mt-6">
+                     <button className="btn btn-primary">Login</button>
+            </div>
+                 <p className="mt-5">Do Not have An Account ? Please <span className="text-blue-500 text-base font-medium"><Link to={'/register'}>Register</Link></span></p>
+             </form>
+        </div>
         </div>
     );
 };
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,40 @@
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
     return (
-        <div>
-            <h1>This is register</h1>
+        <div className="w-1/2 mx-auto border p-10 rounded-lg bg-cyan-200 mt-5">
+            <h1 className="text-4xl font-bold text-center underline text-orange-600 mb-3">Please Register</h1>
+             <form>
+             <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                 </label>
+                    <input type="text" placeholder="name" className="input input-bordered" required />
+             </div>
+             <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Photo Url</span>
+                 </label>
+                    <input type="text" placeholder="photo url" className="input input-bordered" required />
+             </div>
+             <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                 </label>
+                    <input type="email" placeholder="email" className="input input-bordered" required />
+             </div>
+             <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                 </label>
+                   <input type="password" placeholder="password" className="input input-bordered" required />
+             </div>
+             <div className="form-control mt-6">
+                     <button className="btn btn-primary">Register</button>
+            </div>
+                 <p className="mt-5">Already You have An Account Please <span className="text-blue-500 text-base font-medium"><Link to={'/login'}>Login</Link></span></p>
+             </form>
         </div>
     );
 };
